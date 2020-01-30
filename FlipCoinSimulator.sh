9dbe87f1257430eda0995ad2bb4aa3f448c38c97
PalakPartani <partanipalak16@gmail.com>
2020-01-30 11:02:40 +0530

    ADDED:function to calculate percent of singlet combination

--- FlipCoinSimulator.sh
+++ FlipCoinSimulator.sh
@@ -1,13 +1,51 @@
 #!/bin/bash 
 echo "Welcome to flipcoin simulation "
-
+declare -A coinDict
+result=""
+readonly HEAD="H"
+readonly TAIL="T"
+#checking for no of times
 coinFlip()
 {
-		if(( $((RANDOM%2))==1)) #for single time
-		then
-			echo "Head"
-		else
-			echo "Tail"
-		fi
+	for((i=1;i<=$num;i++))
+		do
+			result=""
+			for((j=1;j<=$coin;j++))
+			do
+				if (($((RANDOM%2))==1)) #for multiple times
+				then			
+					result=$result$HEAD	
+				else
+					result=$result$TAIL
+				fi			
+			done
+			coinDict[$result]=$((${coinDict[$result]}+1))
+		done
+		echo "${!coinDict[@]}"
+		echo "${coinDict[@]}"
+		calculatePercent
+
+
 }
-coinFlip
+	
+calculatePercent()
+{
+	for i in ${!coinDict[@]}
+	do
+		value=${coinDict[$i]}
+		coinDict[$i]=$( echo "scale=2; $value * 100 / $num" | bc )
+	done
+}
+read -p "Enter a how many times for flipping a coin : " num
+read -p "Enter the number of coin " coin
+case $coin in
+	1)
+		echo "This is singlet"
+		coinFlip
+		
+		echo "Percentage are : " ${coinDict[@]}
+		;;
+	*)
+		echo "Error"
+	
+esac
